b9be2eba69d3676b7146f8ed5e056b227878d256
Ophir LOJKINE <pere.jobs@gmail.com>
2020-03-06 22:13:42 +0100

    make the data download less verbose

--- download_historic_data.sh
+++ download_historic_data.sh
@@ -5,7 +5,7 @@ set -e
 base="https://github.com/lovasoa/historique-velib-opendata/releases/download/latest"
 outfile=stations.zip
 
-wget -O $outfile "$base/stations.zip" || wget -O $outfile "$base/stations-old.zip"
+wget -nv -O $outfile "$base/stations.zip" || wget -nv -O $outfile "$base/stations-old.zip"
 
 unzip -q -o $outfile -d stations
 
