ee2cfa7bf6c539b1437c19e248ce1fab24a803a9
Ophir LOJKINE <pere.jobs@gmail.com>
2020-03-06 21:59:28 +0100

    Stop early if old data cannot be retrieved

--- download_historic_data.sh
+++ download_historic_data.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 wget -O stations.zip 'https://github.com/lovasoa/historique-velib-opendata/releases/download/latest/stations.zip'
-unzip -q -o stations.zip -d stations && rm stations.zip
+
+unzip -q -o stations.zip -d stations
+
+rm stations.zip
