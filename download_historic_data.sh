d4dc05c1ee636e1b906fb9543e7e9720d7a5c148
Ophir LOJKINE <pere.jobs@gmail.com>
2020-03-24 18:50:46 +0100

    retry history downloads on failure

--- download_historic_data.sh
+++ download_historic_data.sh
@@ -3,7 +3,11 @@
 set -e
 
 base="https://api.github.com/repos/lovasoa/historique-velib-opendata/releases/latest"
-urls=$(curl "$base" | jq -r '.assets|sort_by(.updated_at)|reverse[]|.browser_download_url')
+urls=$(
+    curl --fail --retry 8 --retry-delay 0 "$base" |
+    jq -r '.assets|sort_by(.updated_at)|reverse[]|.browser_download_url'
+)
+
 outfile=stations.zip
 for url in $urls; do
     wget -nv -O "$outfile" "$url" && \
