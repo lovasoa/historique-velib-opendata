1d8b2f501abbd37950b7d4db5db31321afcc62f6
Ophir LOJKINE <pere.jobs@gmail.com>
2020-03-25 11:51:00 +0100

    use github token on history download

--- download_historic_data.sh
+++ download_historic_data.sh
@@ -3,15 +3,18 @@
 set -e
 
 base="https://api.github.com/repos/lovasoa/historique-velib-opendata/releases/latest"
+
+AUTH=$([ $GITHUB_TOKEN ] && echo "-HAuthorization: token $GITHUB_TOKEN")
+
 urls=$(
-    curl --fail --retry 8 --retry-delay 0 "$base" |
+    curl $AUTH --fail --retry 8 --retry-delay 0 "$base" |
     jq -r '.assets|sort_by(.updated_at)|reverse[]|.browser_download_url'
 )
 
 outfile=stations.zip
 for url in $urls; do
-    wget -nv -O "$outfile" "$url" && \
-    unzip -o $outfile && \
+    curl $AUTH --fail --retry 8 --retry-delay 0 --output "$outfile" "$url" &&
+    unzip -o $outfile &&
     break || continue
 done
 
