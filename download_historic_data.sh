1fa61c4971dd26164dbe1c3d327a9e2788a4174a
Ophir LOJKINE <pere.jobs@gmail.com>
2020-03-25 11:58:45 +0100

    remove authentication on download

--- download_historic_data.sh
+++ download_historic_data.sh
@@ -4,16 +4,14 @@ set -e
 
 base="https://api.github.com/repos/lovasoa/historique-velib-opendata/releases/latest"
 
-AUTH=$([ $GITHUB_TOKEN ] && echo "-HAuthorization: token $GITHUB_TOKEN")
-
 urls=$(
-    curl $AUTH --fail --retry 8 --retry-delay 0 "$base" |
+    curl --fail --retry 8 --retry-delay 0 "$base" |
     jq -r '.assets|sort_by(.updated_at)|reverse[]|.browser_download_url'
 )
 
 outfile=stations.zip
 for url in $urls; do
-    curl $AUTH --fail --retry 8 --retry-delay 0 --output "$outfile" "$url" &&
+    curl --fail --retry 8 --retry-delay 0 --output "$outfile" "$url" &&
     unzip -o $outfile &&
     break || continue
 done
