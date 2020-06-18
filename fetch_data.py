fac59787a80961a0b9d22b839f39df5da81a20e6
Ophir LOJKINE <pere.jobs@gmail.com>
2020-06-18 13:56:47 +0200

    gestion des codes station qui ne sont pas des nombres

--- fetch_data.py
+++ fetch_data.py
@@ -9,7 +9,7 @@ import requests
 
 def get_stations():
     sapi="https://velib-metropole-opendata.smoove.pro/opendata/Velib_Metropole/station_information.json"
-    stations_df = pd.DataFrame(requests.get(sapi).json()["data"]["stations"]).astype({"stationCode":int})
+    stations_df = pd.DataFrame(requests.get(sapi).json()["data"]["stations"])
     stations_df.set_index("stationCode", inplace=True)
     stations_df.sort_index(inplace=True)
     stations_df['station_geo'] = ["{:.5f},{:.5f}".format(s.lat,s.lon) for s in stations_df.itertuples()]
@@ -22,7 +22,7 @@ def get_statuses():
     j = requests.get(sapi).json()["data"]["stations"]
     df = pd.DataFrame(j)
     df["last_reported"] = df["last_reported"]
-    df = df.astype({'is_renting':bool, 'stationCode':int,'last_reported':'datetime64[s]'})
+    df = df.astype({'is_renting':bool, 'stationCode':str,'last_reported':'datetime64[s]'})
     df = df.join(df["num_bikes_available_types"].apply(lambda l: pd.Series({**l[0], **l[1]})))
     df = df[["stationCode", "mechanical", "ebike", "is_renting", "last_reported"]]
     df.rename({
